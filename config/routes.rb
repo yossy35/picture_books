b3a9b03728253e0be3384d769655a0d3e06c96fa
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-02-26 23:26:38 +0000

    [Add]controller_homes/registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'registrations/new'
+  get 'registrations/create'
+  root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
