605064dfc83ebcd5b0142e190aad9ff9b2ba792c
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-03-04 23:24:06 +0000

    [Add]new_book_path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     devise_scope :user do
       post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
     end
+
+    get 'users/mypage', to: 'users#mypage'
     
     get 'sessions/new'
     get 'registrations/new'
