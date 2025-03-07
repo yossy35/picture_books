721c2762d96625aadd776ffd18a0fb9e4cf5e8ff
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-03-07 10:04:06 +0000

    [Add]mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #def after_sign_in_path_for(resource)
-  #  user_mypage
-  #end
+  def after_sign_in_path_for(resource)
+    users_mypage_path
+  end
 
   protected
 
