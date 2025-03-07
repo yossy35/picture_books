3a01467ed9542a7a29938887d03e9a769ffac430
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-03-07 09:35:43 +0000

    [Error]create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
   end
 
   def index
