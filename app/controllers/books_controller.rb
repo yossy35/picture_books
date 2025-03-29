dd751124541115a1b5c5ab7d28b889dde5da47c4
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-03-29 01:33:05 +0000

    [add]@book/update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
