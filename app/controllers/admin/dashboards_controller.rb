911b8a7951c26047d00c5d1ee9369c6ea9857ac9
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-04-18 05:29:20 +0000

    admin_review/destroy

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,8 +1,15 @@
 class Admin::DashboardsController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin!
-    def index
-        @users = User.all
-        @books = Book.all
-    end
+  def index
+      @users = User.all
+      @books = Book.all
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to admin_dashboards_path
+  end
+
 end
