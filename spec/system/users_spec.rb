f686d32f9458c87be4b9f208745c543f09d95162
EC2 Default User <yoshino.tsuyoshi@gmail>
2025-06-26 03:24:43 +0000

    投稿者の遷移先の確認

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe 'ユーザー一覧のテスト', type: :system do
   let(:user) { create(:user) }
-  # let!(:book) { create(:book, user: user, ) }
+  let!(:book) { create(:book, user: user, ) }
 
   context '表示とリンク先の確認' do
     before do 
@@ -15,6 +15,11 @@ describe 'ユーザー一覧のテスト', type: :system do
       expect(page).to have_content user.name
       expect(page).to have_link user.name
       expect(page).to have_content user.introduction
+    end
+    it '投稿者の遷移先はユーザーの詳細画面か' do
+      user_link = find_all('a')[1]
+      find("a[href='/users/#{user.id}']").click
+      expect(current_path).to eq('/users/' + user.id.to_s)
     end
   end
 end
